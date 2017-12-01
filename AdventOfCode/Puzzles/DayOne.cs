4a56ffd9071d2365397b3ea209932cdf28d2266b
aellis21 <andr3w3llis@gmail.com>
2017-12-01 21:48:55 +1100

    cleaned up the code a bit

--- AdventOfCode/Puzzles/DayOne.cs
+++ AdventOfCode/Puzzles/DayOne.cs
@@ -1,20 +1,15 @@
 ﻿using System;
-using System.Linq;
 
 namespace AdventOfCode.Puzzles
 {
     public class DayOne
     {
-        public int PuzzleOne(char[] input)
+        public static int PuzzleOne(char[] input)
         {
             var output = 0;
             for (int i = 0; i < input.Length; i++)
             {
-                if (i == input.Length - 1 && input.Last() == input.First())
-                {
-                    output += Int32.Parse(input[i].ToString());
-                }
-                else if (input[i] == input[i + 1])
+                if (input[i] == input[(i+1) % input.Length])
                 {
                     output += Int32.Parse(input[i].ToString());
                 }
@@ -22,21 +17,12 @@ namespace AdventOfCode.Puzzles
             return output;
         }
 
-        public int PuzzleTwo(char[] input)
+        public static int PuzzleTwo(char[] input)
         {
             var output = 0;
             for (int i = 0; i < input.Length; i++)
             {
-                var u = i + input.Length / 2;
-                if (u >= input.Length)
-                {
-                    u = u - input.Length;
-                }
-                if (i == input.Length - 1 && input.Last() == input[u])
-                {
-                    output += Int32.Parse(input[i].ToString());
-                }
-                else if (input[i] == input[u])
+                if (input[i] == input[(i + input.Length / 2) % input.Length])
                 {
                     output += Int32.Parse(input[i].ToString());
                 }
