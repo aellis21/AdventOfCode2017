38a263590adb7f7c44d0a44fe167b3a75d1a5a12
aellis21 <andr3w3llis@gmail.com>
2017-12-04 21:15:32 +1100

    Finished Day 4

---
+++ AdventOfCode/Puzzles/Day3.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Puzzles
+{
+    public class Day3
+    {
+        public static int PuzzleOne(int input)
+        {
+            var result = 0;
+
+            var spiral = CreateSpiral(input);
+
+
+
+            return result;
+        }
+
+        public static int PuzzleTwo(string input)
+        {
+            var result = 0;
+
+            return result;
+        }
+
+        private static int[,] CreateSpiral(int number)
+        {
+            var position = new { x = -1, y = 0 };
+            var directions = new[] {
+                new { x = 1, y = 0 },
+                new { x = 0, y = 1 },
+                new { x = -1, y = 0 },
+                new { x = 0, y = -1 }
+            };
+
+            var sequence = (
+                from n in Enumerable.Range(1, number)
+                from o in Enumerable.Repeat(n, n != number ? 2 : 1)
+                select o
+            ).Reverse().ToList();
+
+            var result = new int[number, number];
+
+            for (int i = 0, current = 1; i < sequence.Count; i++)
+            {
+                var direction = directions[i % directions.Length];
+
+                for (int j = 0; j < sequence[i]; j++, current++)
+                {
+                    position = new
+                    {
+                        x = position.x + direction.x,
+                        y = position.y + direction.y
+                    };
+
+                    result[position.y, position.x] = current;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AdventOfCode/Puzzles/Day4.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Puzzles
+{
+    public class Day4
+    {
+        public static int PuzzleOne(string input)
+        {
+            var result = 0;
+            var lines = input.Split('\n', '\r').Distinct().Where(w => w != string.Empty).ToList();
+
+            foreach (var line in lines)
+            {
+                var words = line.Split(' ');
+                result = words.Distinct().Count() == words.Count() ? result + 1 : result;
+            }
+
+            return result;
+        }
+
+        public static int PuzzleTwo(string input)
+        {
+            var result = 0;
+            var lines = input.Split('\n', '\r').Distinct().Where(w => w != string.Empty).ToList();
+
+            foreach (var line in lines)
+            {
+                var words = line.Split(' ').ToList();
+
+                words = words.Select(w => OrderString(w)).ToList();
+
+                result = words.Distinct().Count() == words.Count() ? result + 1 : result;
+            }
+
+            return result;
+        }
+
+        private static string OrderString(string word)
+        {
+            var charArray = word.ToCharArray();
+            var result = charArray.OrderByDescending(l => l).ToArray();
+            return new string(result);
+        }
+    }
+}
